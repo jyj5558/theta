b1b99e1e4986baa06c222bf41b26d85903235757
black1414 <63075313+black1414@users.noreply.github.com>
2021-01-15 07:51:29 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -40,5 +40,12 @@ echo " "
 echo Are all downloaded files named correctly?
 ls -lh $species/GCF*
 
+echo Now, the reference will be searched for a mitochondrion sequence
+grep "mitochondrion" ./$species/${species}_ref/${species}.genomic.fna | cut -f 1 
+echo " "
+echo If a sequence was printed out, this will now be removed from the reference genome
+
+filterbyname.sh include=t in=./$species/${species}_ref/${species}.genomic.fna  out=test.fasta names="mitochondrion" ow=t substring=t
+
 echo " "
 echo DONE, please move on to the SRR download step
