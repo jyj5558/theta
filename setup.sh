e4993fbe62965efb985154c68e629b8a79371180
black1414 <63075313+black1414@users.noreply.github.com>
2021-01-27 04:16:24 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,11 +2,11 @@
 
 #load modules
 ml bioinfo BBMap/37.93
-
+echo " - - - -" ;sleep 2; echo " - - - -" ;sleep 2 ; echo " - - - -" ;sleep 2
+echo "٩(^‿^)۶"
 echo Hello, what is the name of the species genome you are about to download?
 read species
-echo Creating directory structure for $species, please confirm that it was created correctly
-
+echo Creating directory structure for $species
 mkdir -p ./$species/${species}_ref
 mkdir ./$species/${species}_rm
 mkdir ./$species/${species}_gtf
@@ -15,39 +15,47 @@ mkdir ./$species/sra/raw
 mkdir ./$species/sra/cleaned
 mkdir ./$species/sra/aligned
 
+echo " - - - -" ;sleep 2; echo " - - - -" ;sleep 2 ; echo " - - - -" ;sleep 2
 echo Please specify the path to the FTP refseq genome:
 read ref
 
+echo " - - - -" ;sleep 2; echo " - - - -" ;sleep 2 ; echo " - - - -" ;sleep 2
 echo Please specify the path to the FTP refseq rm out file:
 read rm
 
+echo " - - - -" ;sleep 2; echo " - - - -" ;sleep 2 ; echo " - - - -" ;sleep 2
 echo Please specify the path to the FTP refseq annotations in GTF format:
 read gtf    
 
-echo Downlaoding reference genome, repeat masker out and annotation files for $species. Please check standard out and files to confirm all downloaded correctly
-wget $ref -O ./$species/${species}_ref/${species}.genomic.fna.gz -o log_ref 
-wget $rm -O ./$species/${species}_rm/${species}.rm.out.gz -o log_rm
-wget $gtf -O ./$species/${species}_gtf/${species}.gtf.gz -o log_gtf
+echo " - - - -" ;sleep 2; echo " - - - -" ;sleep 2 ; echo " - - - -" ;sleep 2
+echo Downloading reference genome, repeat masker out and annotation files for $species
+wget $ref -O ./$species/${species}_ref/${species}.genomic.fna.gz -o ./$species/${species}_ref/log_ref 
+wget $rm -O ./$species/${species}_rm/${species}.rm.out.gz -o ./$species/${species}_rm/log_rm
+wget $gtf -O ./$species/${species}_gtf/${species}.gtf.gz -o ./$species/${species}_gtf/log_gtf
+
 echo Downloads are now complete, decompressing files
-
+echo " - - - -" ;sleep 2; echo " - - - -" ;sleep 2 ; echo " - - - -" ;sleep 2
 gzip -d $species/*/*.gz
-echo " "
+echo " - - - -" ;sleep 2; echo " - - - -" ;sleep 2 ; echo " - - - -" ;sleep 2
 
-echo Please confirm that all downloaded files were fully downloaded 100%
-cat log* | grep "100%" 
-echo " "
+echo Please confirm that all downloaded files were fully downloaded 100% below
+echo " - - - -" ;sleep 2; echo " - - - -" ;sleep 2 ; echo " - - - -" ;sleep 2
+cat ./$species/${species}_*/log* | grep "100%" 
+echo " - - - -" ;sleep 2; echo " - - - -" ;sleep 2 ; echo " - - - -" ;sleep 2
 
-echo Are all downloaded files named correctly and are not empty?
+echo Please double check that all downloaded files are named correctly below and are not empty
 ls -lh $species/GCF*
-echo " "
-
+echo " - - - -" ;sleep 2; echo " - - - -" ;sleep 2 ; echo " - - - -" ;sleep 2
 echo Now, the reference will be searched for a mitochondrion sequence
+echo " - - - -" ;sleep 2; echo " - - - -" ;sleep 2 ; echo " - - - -" ;sleep 2
 grep "mitochondrion" ./$species/${species}_ref/${species}.genomic.fna | cut -f 1 
-echo " "
-echo If a sequence was printed out, this will now be removed from the reference genome
-
+echo " - - - -" ;sleep 2; echo " - - - -" ;sleep 2 ; echo " - - - -" ;sleep 2
+echo If a sequence was printed out above, this will now be removed from the reference genome
+echo " - - - -" ;sleep 2; echo " - - - -" ;sleep 2 ; echo " - - - -" ;sleep 2
 filterbyname.sh include=f in=./$species/${species}_ref/${species}.genomic.fna out=./$species/${species}_ref/tmp.fasta names="mitochondrion" ow=t substring=t
 rm ./$species/${species}_ref/${species}.genomic.fna
 mv ./$species/${species}_ref/tmp.fasta ./$species/${species}_ref/${species}.genomic.fna
-echo " "
+echo " - - - -" ;sleep 2; echo " - - - -" ;sleep 2 ; echo " - - - -" ;sleep 2
 echo DONE-please check all files before moving on to the SRR download step
+echo " "
+echo "┏(-_-)┛┗(-_-<feff> )┓┗(-_-)┛┏(-_-)┓"
