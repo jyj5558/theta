3e8f5b291032c6baa0a0fa202e8e1913a79069d3
black1414 <63075313+black1414@users.noreply.github.com>
2021-02-05 10:47:39 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,16 +4,24 @@
 ml bioinfo BBMap/37.93
 echo " - - - -" ;sleep 2; echo " - - - -" ;sleep 2 ; echo " - - - -" ;sleep 2
 echo "٩(^‿^)۶"
-echo Hello, what is the name of the species genome you are about to download?
+echo Hello, what is the name of the accesion for the species genome you are about to download?
 read species
 echo Creating directory structure for $species
 mkdir -p ./$species/${species}_ref
 mkdir ./$species/${species}_rm
 mkdir ./$species/${species}_gtf
+
 mkdir ./$species/sra
-mkdir ./$species/sra/raw
-mkdir ./$species/sra/cleaned
-mkdir ./$species/sra/aligned
+mkdir ./$species/sra/two_ch
+mkdir ./$species/sra/four_ch
+
+mkdir ./$species/sra/two_ch/raw
+mkdir ./$species/sra/two_ch/cleaned
+mkdir ./$species/sra/two_ch/aligned
+
+mkdir ./$species/sra/four_ch/raw
+mkdir ./$species/sra/four_ch/cleaned
+mkdir ./$species/sra/four_ch/aligned
 
 echo " - - - -" ;sleep 2; echo " - - - -" ;sleep 2 ; echo " - - - -" ;sleep 2
 echo Please specify the path to the FTP refseq genome:
