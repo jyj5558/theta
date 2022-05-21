1c2894af2520da92d92a0f6337108a6b20dd8fc0
Jong Yoon Jeon <jeon96@purdue.edu>
2022-05-20 21:37:38 -0400

    Update step3_mapping.sh

--- step3_mapping.sh
+++ step3_mapping.sh
@@ -7,6 +7,8 @@
 #SBATCH -e %x_%j.err
 #SBATCH -o %x_%j.out
 #SBATCH --mem=20GB
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user="your email address (e.g.jeon96@purdue.edu) without quotation marks"
 
 
 module load bioinfo
