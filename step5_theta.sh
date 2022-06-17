3db04500a7e46c6e6e9e94c965fa29c506b860cd
Jong Yoon Jeon <jeon96@purdue.edu>
2022-06-17 12:49:13 -0400

    Update step5_theta.sh

--- step5_theta.sh
+++ step5_theta.sh
@@ -3,7 +3,7 @@
 #SBATCH -A fnrquail
 #SBATCH -t 10-00:00:00 
 #SBATCH -N 1 
-#SBATCH -n 40 
+#SBATCH -n 64 
 #SBATCH -e %x_%j.err
 #SBATCH -o %x_%j.out
 #SBATCH --mail-type=END,FAIL
@@ -70,21 +70,27 @@ awk '{print $1"\t"$2+1"\t"$3}' $PD/ok.bed > ./angsd.file
 angsd sites index ./angsd.file
 
 # estimate GL. Increase -P if you can/should employ more CPUs.
-angsd -P 40 -bam ./bam.filelist -sites ./angsd.file -anc $PD/*_ref/ref.fa \
+echo "Genotype likelihood estimation started"
+angsd -P 64 -bam ./bam.filelist -sites ./angsd.file -anc $PD/*_ref/ref.fa \
 -ref $PD/*_ref/ref.fa -dosaf 1 -gl 1 -remove_bads 1 -only_proper_pairs 1 \
 -minMapQ 30 -minQ 30 -rf $PD/*_ref/chrs.txt -minInd $MIND -out out
+echo "Genotype likelihood estimation done"
 
 # obtain ML estimate of SFS using the folded realSFS. Increase -P if you can/should employ more CPUs.
-realSFS -P 40 out.saf.idx  -fold 1 > out.sfs 
+echo "SFS estimation started"
+realSFS -P 64 out.saf.idx  -fold 1 > out.sfs
+echo "SFS estimation done"
 
 # calculate theta for each site. Increase -P if you can/should employ more CPUs.
-realSFS saf2theta  -P 40 out.saf.idx -sfs out.sfs -outname out
+realSFS saf2theta  -P 64 out.saf.idx -sfs out.sfs -outname out
 
 # estimate 
 thetaStat print out.thetas.idx > out.thetas_persite.txt
+echo "per-site Theta file created"
 
 #Sliding window estimate
 thetaStat do_stat out.thetas.idx -win 50000 -step 10000  -outnames theta.thetasWindow.gz
+echo "window-based Theta file created"
 
 # column 4 has Wattersons, column 9 has Tajima's D, and column 10 has Fu & Li's Fs
 awk '{print $4}' theta.thetasWindow.gz.pestPG > Watterson
@@ -111,11 +117,12 @@ echo -e "$PWD\t $meanD\t $sdD" \
 >> TajimaD_${genus_species}.txt
 echo -e "$PWD\t $meanF\t $sdF" \
 >> FuF_${genus_species}.txt
+echo "Theta, D, F txt created"
 
 ####################################
 # heterozygosity for each individual
 ####################################
-#Increase -P if you can/should employ more CPUs.
+#Increase "-P"s if you can/should employ more CPUs.
 
 mkdir ./HET
 OUTDIR='HET'
@@ -123,16 +130,19 @@ OUTDIR='HET'
 ls -1 ../sra/final_bams/*bam | sed 's/\//\'$'\t/g' | cut -f 4| sed 's/.bam//g' | while read -r LINE
 
 do
+echo "${LINE} heterozygosity estimation started"
 
 angsd -i ../sra/final_bams/${LINE}.bam -ref $PD/*_ref/ref.fa -anc $PD/*_ref/ref.fa -dosaf 1 -rf $PD/*_ref/chrs.txt -sites ./angsd.file \
--minMapQ 30 -minQ 30 -P 40 -out ${OUTDIR}/${LINE} -only_proper_pairs 1 -baq 2 \
+-minMapQ 30 -minQ 30 -P 64 -out ${OUTDIR}/${LINE} -only_proper_pairs 1 -baq 2 \
 -GL 2 -doMajorMinor 1 -doCounts 1 -setMinDepthInd 5 -uniqueOnly 1 -remove_bads 1 
 
-realSFS -P 40 -fold 1 ${OUTDIR}/${LINE}.saf.idx > ${OUTDIR}/est.ml
+realSFS -P 64 -fold 1 ${OUTDIR}/${LINE}.saf.idx > ${OUTDIR}/est.ml
 
+echo "${LINE} heterozygosity is" >> ./Het
 cd $OUTDIR
 Rscript -e 'a<-scan("est.ml"); a[2]/sum(a)' >>  ../Het
 mv est.ml ${LINE}_est.ml
+echo "${LINE} heterozygosity estimation done"
 
 cd ../
 done
@@ -151,6 +161,7 @@ meanH2 += delta * ($2 - avg); } END { print sqrt(meanH2 / NR); }' Het)
 # print to file
 echo -e "$PWD\t $meanH\t $sdH" \
 >> Het_${genus_species}.txt
+echo "Population heterozygosity txt created"
 
 #######
 # ROHs
@@ -158,8 +169,10 @@ echo -e "$PWD\t $meanH\t $sdH" \
 
 angsd -b ./bam.filelist -dobcf 1 -gl 1 -dopost 1 -domajorminor 1 -domaf 1 -snp_pval 1e-6 -P 40
 
+echo "ROH estimation started"
 bcftools query -f'%CHROM\t%POS\t%REF,%ALT\t%INFO/AF\n' angsdput.bcf | bgzip -c > ${genus_species}.freqs.tab.gz
 tabix -s1 -b2 -e2 ${genus_species}.freqs.tab.gz
 bcftools roh --AF-file ${genus_species}.freqs.tab.gz --output ROH_${genus_species}.txt --threads 40 angsdput.bcf
+echo "ROH txt created"
 
 # END
