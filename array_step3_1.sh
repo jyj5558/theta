ee4c83445f167cd14a4e04637800c90c3786a0fd
Jong Yoon Jeon <jeon96@purdue.edu>
2022-08-13 17:24:16 -0400

    Update array_step3_1.sh

--- array_step3_1.sh
+++ array_step3_1.sh
@@ -39,9 +39,6 @@ cd /scratch/bell/dewoody/theta/${genus_species}/sra/cleaned/
 # index reference 
 bwa index -a bwtsw ../../*_ref/ref.fa
 
-# and index reference sequence in preparation for step4
-samtools faidx ../../*_ref/ref.fa
-
 # capture all cleaned fastq files with variable
 ls -1 *.fq | sed "s/_[1-2]_val_[1-2].fq//g" | uniq > cleaned_sralist
 
