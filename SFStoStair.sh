3040b08df05adb8ed394873d6db830a5d09236fc
Jong Yoon Jeon <jeon96@purdue.edu>
2023-03-24 23:20:14 -0400

    Update SFStoStair.sh

--- SFStoStair.sh
+++ SFStoStair.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=stair_Nannospalax-galili
+#SBATCH --job-name=stair_genus-species
 #SBATCH -A fnrpupfish
 #SBATCH -N 1
 #SBATCH -t 14-00:00:00
@@ -7,13 +7,13 @@
 #SBATCH -e %x_%j.err
 #SBATCH -o %x_%j.out
 #SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=jeon96@purdue.edu
+#SBATCH --mail-user=
 
 module --force purge
 module load biocontainers
 module load angsd
 
-genus_species=Nannospalax-galili
+genus_species=
 
 PD=/scratch/bell/dewoody/theta/${genus_species}
 THETA=/scratch/bell/dewoody/theta/${genus_species}/theta
@@ -24,20 +24,20 @@ cd $THETA
 MIND=$((`wc -l < ./bam.filelist` / 2))
 
 # convert bed file to angsd format
-#awk '{print $1"\t"$2+1"\t"$3}' $PD/ok.bed > ./angsd.file
+awk '{print $1"\t"$2+1"\t"$3}' $PD/ok.bed > ./angsd.file
 
 # index file
-#angsd sites index ./angsd.file
+angsd sites index ./angsd.file
 
 # estimate SFS
-#echo "Site frequency spectrum estimation started"
-#angsd -bam ./bam.filelist -ref $PD/*_ref/ref.fa -anc $PD/*_ref/ref.fa -rf $PD/*_ref/chrs.txt -sites ./angsd.file \
-#-dosaf 1 -GL 2 \
-#-minInd $MIND -minMapQ 30 -minQ 30 -only_proper_pairs 1 -remove_bads 1 -uniqueOnly 1  -baq 2 \
-#-out stair -P 64 
+echo "Site frequency spectrum estimation started"
+angsd -bam ./bam.filelist -ref $PD/*_ref/ref.fa -anc $PD/*_ref/ref.fa -rf $PD/*_ref/chrs.txt -sites ./angsd.file \
+-dosaf 1 -GL 2 \
+-minInd $MIND -minMapQ 30 -minQ 30 -only_proper_pairs 1 -remove_bads 1 -uniqueOnly 1  -baq 2 \
+-out stair -P 64 
 
-#realSFS -P 64 stair.saf.idx  -fold 1 > stair.sfs
-#echo "Site frequency spectrum estimation finished"
+realSFS -P 64 stair.saf.idx  -fold 1 > stair.sfs
+echo "Site frequency spectrum estimation finished"
 
 # manually copy and paste numbers in ".sfa" file to ".blueprint" file after making a new ".blueprint" file, e.g., "Orcinus_orca_fold.blueprint"/ 
 # For 'n' diploid samples, the site frequency spectrum (SFS) is the (2n+1) vector containing the proportion of site carrying 'k'-mutations. This means that the first element in the SFS is the proportion of sites where we don't observe any mutations, The second value is the proportion of sites where we observe 1 mutations. The last value is the proportion of sites we only observe mutations. It follows that the first and last column are the invariable categories. For folded sfs, the last half values will be "0".
