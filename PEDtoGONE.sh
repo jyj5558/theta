82d4e3a0ad706e44fb59a6b1012e7d492033f079
Jong Yoon Jeon <jeon96@purdue.edu>
2023-03-24 23:22:16 -0400

    Update PEDtoGONE.sh

--- PEDtoGONE.sh
+++ PEDtoGONE.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=Ne_Orcinus_orca
+#SBATCH --job-name=GONE_genus-species
 #SBATCH -A fnrpredator
 #SBATCH -t 14-00:00:00 
 #SBATCH -N 1 
@@ -15,8 +15,8 @@ module load bioinfo
 module load angsd
 module load plink/1.90b6.4
 
-genus_species=Orcinus_orca
-n=64
+genus_species=
+n=
 
 ########Do not edit beneath this row########
 
@@ -102,36 +102,6 @@ bash script_GONE.sh ${genus_species}_GONE2
 # move back result files to species directory
 mv *_${genus_species}* $THETA
 
-#cd $THETA
-
-# plink file for SNeP
-#angsd -bam bam.filelist -ref $PD/*_ref/ref.fa -rf $PD/*_ref/chrs.txt -sites ./angsd.file -out plink_SNeP \
-#-doPlink 2 -doGeno -4 -doPost 1 -doMajorMinor 1 -GL 2 -doCounts 1 -doMaf 1 -postCutoff 0.99 -SNP_pval 1e-6 -geno_minDepth 5 \
-#-minMapQ 30 -minQ 30 -minInd $IND -only_proper_pairs 1 -remove_bads 1 -uniqueOnly 1 -baq 2 -P $n
-
-# convert to plink .ped and .map format
-#plink --tfile plink_SNeP --recode --allow-extra-chr --out ${genus_species}_SNeP
-
-# create a chromosome code file
-#awk 'BEGIN {OFS=" "}; {print $0, NR}' $PD/*_ref/chrs.txt > chr-codes2.txt
-
-# save a copy of .map file
-#cp ${genus_species}_SNeP.map ${genus_species}_SNePcp.map
-
-# change chromosome code to numerical values
-#awk -vOFS=" " 'FNR==NR{arr[$1]=$2;next}{$1=arr[$1]; print}' chr-codes2.txt ${genus_species}_SNeP.map > ${genus_species}_SNeP2.map
-#mv ${genus_species}_SNeP2.map ${genus_species}_SNeP.map
-#join -o '2.2 1.2 1.3 1.4' $genus_species.map chr-codes.txt
-
-# copy to the directory having SNeP program
-#cp ${genus_species}_SNeP.ped /depot/fnrdewoody/apps/SNeP/${genus_species}_SNeP.ped 
-#cp ${genus_species}_SNeP.map /depot/fnrdewoody/apps/SNeP/${genus_species}_SNeP.map 
-
-# run SNeP
-#cd /depot/fnrdewoody/apps/SNeP/
-#./SNeP -ped ./${genus_species}_SNeP.ped -map ./${genus_species}_SNeP.map
-
-# move back result files to species directory
-#mv *${genus_species}* $THETA
+cd $THETA
 
 # END
