c41f4f71f0170083887518286d7273b24626a54b
Jong Yoon Jeon <jeon96@purdue.edu>
2024-01-13 14:43:53 -0500

    Update SFStoStair.sh to include total nucleic sites calculation

--- SFStoStair.sh
+++ SFStoStair.sh
@@ -29,7 +29,7 @@ awk '{print $1"\t"$2+1"\t"$3}' $PD/ok.bed > ./angsd.file
 # index file
 angsd sites index ./angsd.file
 
-# estimate SFS
+# estimate SFS; or just use out.sfs generated at step 5
 echo "Site frequency spectrum estimation started"
 angsd -bam ./bam.filelist -ref $PD/*_ref/ref.fa -anc $PD/*_ref/ref.fa -rf $PD/*_ref/chrs.txt -sites ./angsd.file \
 -dosaf 1 -GL 2 \
@@ -39,6 +39,7 @@ angsd -bam ./bam.filelist -ref $PD/*_ref/ref.fa -anc $PD/*_ref/ref.fa -rf $PD/*_
 realSFS -P 64 stair.saf.idx  -fold 1 > stair.sfs
 echo "Site frequency spectrum estimation finished"
 
+cat stair.sfs | tr ' ' '\n' | awk -v OFMT=%.17g '{sum+=$1} END {print sum} # total number of observed nucleic sites, including polymorphic and monomorphic (needed for stairway plot)
 # manually copy and paste numbers in ".sfa" file to ".blueprint" file after making a new ".blueprint" file, e.g., "Orcinus_orca_fold.blueprint"/ 
 # For 'n' diploid samples, the site frequency spectrum (SFS) is the (2n+1) vector containing the proportion of site carrying 'k'-mutations. This means that the first element in the SFS is the proportion of sites where we don't observe any mutations, The second value is the proportion of sites where we observe 1 mutations. The last value is the proportion of sites we only observe mutations. It follows that the first and last column are the invariable categories. For folded sfs, the last half values will be "0".
 # For "nrand", we suggest to use 4 numbers, roughly (nseq-2)/4, (nseq-2)/2, (nseq-2)*3/4, nseq-2. The range of the numbers is between 1 and nseq-2. 
