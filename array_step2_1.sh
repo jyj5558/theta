2789928752bb0276f39066254d89a00a673461c2
Jong Yoon Jeon <jeon96@purdue.edu>
2024-03-21 22:58:50 -0400

    Update array_step2_1.sh
    updated to be agnostic

--- array_step2_1.sh
+++ array_step2_1.sh
@@ -6,6 +6,7 @@
 # this script will create:
 # sralist for the downstream analysis
 # directories to store sra files
+# script was written to be run with SLURM job scheduler
 #####################################
 
 ########################
@@ -14,6 +15,7 @@
 
 # define variables
 genus_species=$1
+THETA=/scratch/bell/dewoody/theta
 
 # create directories and download SRAs
 mkdir -p ./sra/raw
@@ -21,7 +23,7 @@ mkdir -p ./sra/cleaned
 mkdir -p ./sra/aligned
 
 # extract releveant info from metadata file in theta directory for target species and save in species folder:
-cd /scratch/bell/dewoody/theta/${genus_species}/
+cd ${THETA}/${genus_species}/
 cat $CLUSTER_SCRATCH/theta/SRA_metadata/${genus_species}.txt | sed 's/ /_/g'  > ${genus_species}_SRA.txt
 cat ${genus_species}_SRA.txt | cut -f 1 | tail -n +2 > ./sra/raw/sralist
 
