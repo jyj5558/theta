32a25bcbd22ccc1b75d3a44c3225a78f9e92ecba
Jong Yoon Jeon <jeon96@purdue.edu>
2024-03-21 23:15:15 -0400

    Update array_step2_cat.sh
    updated to be agnostic

--- array_step2_cat.sh
+++ array_step2_cat.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+# script was written to be run with SLURM job scheduler
+
 # define variables
 genus_species=$1
 job_id1=$2
 job_id2=$3
 
 # move to the directory
-cd /scratch/bell/dewoody/theta/${genus_species}
+cd /path/to/theta/${genus_species}
 
 # concatenate .err and .out files
 for f in `ls S2_1_${genus_species}_${job_id1}.err S2_2_${genus_species}_${job_id2}_[0-9]*.err | sort`
