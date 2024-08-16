885a27e66c9e142afb49fb90c34d0f0195490a53
Jong Yoon Jeon <jeon96@purdue.edu>
2024-08-15 22:04:59 -0400

    Update qc_reference_stats.R

--- source/qc_reference_stats.R
+++ source/qc_reference_stats.R
@@ -1,6 +1,7 @@
 pwd = commandArgs(trailingOnly=TRUE)[2]
 g_s = commandArgs(trailingOnly=TRUE)[3]
 acc = commandArgs(trailingOnly=TRUE)[4]
+dir = commandArgs(trailingOnly=TRUE)[5]
 
 setwd(pwd)
 
@@ -10,7 +11,7 @@ setwd(pwd)
 
 
 
-a <- read.table(paste("/scratch/bell/dewoody/theta/", g_s, "/", acc, "_rm/nonrepeat.bed", sep=""), header = F)
+a <- read.table(paste(dir, g_s, "/", acc, "_rm/nonrepeat.bed", sep=""), header = F)
 
 # make header
 names(a) <- c("chr","start","stop")
@@ -21,13 +22,13 @@ a$diff <- (a$stop - a$start)
 # sum colum 4
 ok <- sum(a$diff)
 
-write.table(ok, file = paste("/scratch/bell/dewoody/theta/", g_s, "/", "norepeat.txt", sep=""), row.names = F, col.names = F)
+write.table(ok, file = paste(dir, g_s, "/", "norepeat.txt", sep=""), row.names = F, col.names = F)
 
 #################
 # estimate number of sites with mappability == 1
 #################
 
-a <- read.table(paste("/scratch/bell/dewoody/theta/", g_s, "/mappability/mappability.bed", sep=""), header = F)
+a <- read.table(paste(dir, g_s, "/mappability/mappability.bed", sep=""), header = F)
 
 # make header
 names(a) <- c("chr","start","stop")
@@ -38,13 +39,13 @@ a$diff <- (a$stop - a$start)
 # sum colum 4
 okmap <- sum(a$diff)
 
-write.table(okmap, file = paste("/scratch/bell/dewoody/theta/", g_s, "/", "okmap.txt", sep=""), row.names = F, col.names = F)
+write.table(okmap, file = paste(dir, g_s, "/", "okmap.txt", sep=""), row.names = F, col.names = F)
 
 ################
 # estimate number of sites with ok mappability and no repeats
 #################
 
-a <- read.table(paste("/scratch/bell/dewoody/theta/", g_s, "/", "ok.bed", sep=""), header = F)
+a <- read.table(paste(dir, g_s, "/", "ok.bed", sep=""), header = F)
 
 # make header
 names(a) <- c("chr","start","stop")
@@ -55,6 +56,6 @@ a$diff <- (a$stop - a$start)
 # sum colum 4
 ok <- sum(a$diff)
 
-write.table(ok, file = paste("/scratch/bell/dewoody/theta/", g_s, "/", "okbed.txt", sep=""), row.names = F, col.names = F)
+write.table(ok, file = paste(dir, g_s, "/", "okbed.txt", sep=""), row.names = F, col.names = F)
 
 # END
