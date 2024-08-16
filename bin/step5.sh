c8b383cca8829610064e9b566a2252fca284097a
Jong Yoon Jeon <jeon96@purdue.edu>
2024-08-16 12:37:35 -0400

    Update step5.sh

--- bin/step5.sh
+++ bin/step5.sh
@@ -48,17 +48,17 @@ echo "Genotype likelihood estimation started"
 angsd -bam ./bam.filelist -ref $PD/*_ref/ref.fa -anc $PD/*_ref/ref.fa -rf $PD/*_ref/chrs.txt -sites ./angsd.file \
 -dosaf 1 -GL 2 -doMajorMinor 1 \
 -minInd $MIND -minMapQ 30 -minQ 30 -only_proper_pairs 1 -remove_bads 1 -uniqueOnly 1  -baq 2 \
--out out -P $n 
+-out out -P 64 
 echo "Genotype likelihood estimation done"
 
 # obtain ML estimate of SFS using the folded realSFS
 echo "SFS estimation started"
-realSFS -P $n out.saf.idx  -fold 1 > out.sfs
+realSFS -P 64 out.saf.idx  -fold 1 > out.sfs
 echo "SFS estimation done"
 
 # calculate theta for each site
 echo "Theta estimation started"
-realSFS saf2theta out.saf.idx -sfs out.sfs -outname out -P $n 
+realSFS saf2theta out.saf.idx -sfs out.sfs -outname out -P 64 
 
 # estimate 
 thetaStat print out.thetas.idx > out.thetas_persite.txt
@@ -98,9 +98,9 @@ echo "${LINE} heterozygosity estimation started"
 angsd -i ../sra/final_bams/${LINE}.bam -ref $PD/*_ref/ref.fa -anc $PD/*_ref/ref.fa -rf $PD/*_ref/chrs.txt -sites ./angsd.file \
 -dosaf 1 -GL 2 -doMajorMinor 1 \
 -minMapQ 30 -minQ 30 -only_proper_pairs 1 -remove_bads 1 -uniqueOnly 1 -baq 2 -doCounts 1 -setMinDepthInd 5 \
--out ${OUTDIR}/${LINE} -P $n
+-out ${OUTDIR}/${LINE} -P 64
  
-realSFS -P $n -fold 1 ${OUTDIR}/${LINE}.saf.idx > ${OUTDIR}/${LINE}_est.ml
+realSFS -P 64 -fold 1 ${OUTDIR}/${LINE}.saf.idx > ${OUTDIR}/${LINE}_est.ml
 
 cd ${OUTDIR}
 Rscript -e 'args<-commandArgs(TRUE); LINE<-args[1]; a<-scan(paste(LINE,"est.ml", sep="_")); a[2]/sum(a)' ${LINE} >>  ../Het
@@ -133,13 +133,13 @@ echo "Population heterozygosity .txt created"
 echo "File conversion to bcf started"
 angsd -b ./bam.filelist -ref $PD/*_ref/ref.fa -rf $PD/*_ref/chrs.txt -sites ./angsd.file \
 -dobcf 1 -gl 2 -dopost 1 -domajorminor 1 -domaf 1 \
--minMapQ 30 -minQ 30 -only_proper_pairs 1 -remove_bads 1 -uniqueOnly 1  -baq 2 -snp_pval 1e-6 -P $n
+-minMapQ 30 -minQ 30 -only_proper_pairs 1 -remove_bads 1 -uniqueOnly 1  -baq 2 -snp_pval 1e-6 -P 64
 echo "bcf file created"
 
 echo "ROH estimation started"
 bcftools query -f'%CHROM\t%POS\t%REF,%ALT\t%INFO/AF\n' angsdput.bcf | bgzip -c > ${genus_species}.freqs.tab.gz
 tabix -s1 -b2 -e2 ${genus_species}.freqs.tab.gz
-bcftools roh --AF-file ${genus_species}.freqs.tab.gz --output ROH_${genus_species}_PLraw.txt --threads $n angsdput.bcf
+bcftools roh --AF-file ${genus_species}.freqs.tab.gz --output ROH_${genus_species}_PLraw.txt --threads 64 angsdput.bcf
 echo "ROH raw files created"
 
 echo "ROH raw file parsing started"
